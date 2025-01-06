27398975550b2298d784ac01f0dcdb29cc00f045
Patrik Munzar <patrikmunzar@seznam.cz>
2025-01-06 19:49:48 +0100

    Přidejte soubory projektu.

---
+++ MyProject.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.Web.Models;
+
+namespace MyProject.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Igor Hnízdo",
+                Cipher = 123
+            };
+            return View(data);
+        }
+    }
+}
---
+++ MyProject.Web/Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyProject.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public int? Cipher { get; set; }
+    }
+}

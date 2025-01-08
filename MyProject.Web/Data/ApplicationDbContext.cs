e3ffb5a613cf56b2f9c8f72c7618796857f2ca53
Patrik Munzar <patrikmunzar@seznam.cz>
2025-01-08 13:58:08 +0100

    Completed LM module

--- MyProject.Web/Data/ApplicationDbContext.cs
+++ MyProject.Web/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LeaveManagementSystem.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace MyProject.Web.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<LeaveType> LeaveTypes { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- MyProject.Web/Data/Migrations/20250107160928_AddingLeaveTypesTable.Designer.cs
+++ MyProject.Web/Data/Migrations/20250107160928_AddingLeaveTypesTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyProject.Web.Data;
 
@@ -11,9 +12,11 @@ using MyProject.Web.Data;
 namespace MyProject.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250107160928_AddingLeaveTypesTable")]
+    partial class AddingLeaveTypesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
